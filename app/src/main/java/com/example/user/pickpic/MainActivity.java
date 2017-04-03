4be0990c3c5abb719ce4e22e4703bccf84877b83
vkdjdldnjsw <vkdnjdldnjsw@naver.com>
2017-04-03 22:22:43 +0900

    set icon color change

--- app/src/main/java/com/example/user/pickpic/MainActivity.java
+++ app/src/main/java/com/example/user/pickpic/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.user.pickpic;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.*;
 import android.support.v4.view.PagerAdapter;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.ic_access_time_black_48dp));
         tabLayout.addTab(tabLayout.newTab().setIcon(
                 R.drawable.ic_local_offer_black_48dp));
+        tabLayout.getTabAt(0).getIcon().setColorFilter(Color.parseColor("#424242"), PorterDuff.Mode.SRC_IN);
+        for(int i = 1; i< tabLayout.getTabCount(); i++){
+            tabLayout.getTabAt(i).getIcon().setColorFilter(Color.parseColor("#a8a8a8"), PorterDuff.Mode.SRC_IN);
+        }
 
         final ViewPager viewPager =
                 (ViewPager) findViewById(R.id.pager);
@@ -42,11 +49,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
+                tab.getIcon().setColorFilter(Color.parseColor("#424242"), PorterDuff.Mode.SRC_IN);
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
+                tab.getIcon().setColorFilter(Color.parseColor("#a8a8a8"), PorterDuff.Mode.SRC_IN);
             }
 
             @Override
